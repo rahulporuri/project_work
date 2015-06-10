c30930791cf3c4b10188d3f796faa79ebc6a6956
rahul.poruri <rahul.poruri@gmail.com>
2015-06-10 21:54:26 +0530

    cleanup and working code v1.0

---
+++ bouncing_model/evolve_hk_test_k3.py
@@ -0,0 +1,101 @@
+import numpy as numpy
+import matplotlib.pyplot as plt
+import scipy.optimize as opt
+
+def DDhk(N,hk0,Dhk0):
+    return -((3*N -1./N +(DH(N)/H(N)))*Dhk0 +(((k0*N)/(aN(N)*H(N)))**2)*hk0)
+
+def rk4_step(N, hk0, Dhk0, step):
+    F1 = Dhk0
+    f1 = DDhk(N, hk0, Dhk0)
+    F2 = Dhk0 +f1*step/2.
+    f2 = DDhk(N +step/2., hk0 +F1*step/2., Dhk0 +f1*step/2.)
+    F3 = Dhk0 +f2*step/2.
+    f3 = DDhk(N +step/2., hk0 +F2*step/2., Dhk0 +f2*step/2.)
+    F4 = Dhk0 +f3*step
+    f4 = DDhk(N +step, hk0 +F3*step, Dhk0 +f3*step)
+
+    return numpy.array([(f1 +2*f2 +2*f3 +f4)*step/6.], dtype=complex), numpy.array([(F1 +2*F2 +2*F3 +F4)*step/6.], dtype=complex) # [Dhk, hk] update
+
+A = lambda N : a0*numpy.exp(N**2/2.)
+an = lambda n : a0*((1.+(n/n0)**2)**p)
+aN = lambda N : an(n(N))
+h = lambda n : (2.*a0*n/n0**2)*(1./an(n)**2)
+H = lambda N : h(n(N))
+
+fn = lambda n : (2.*a0/n0**2)*(1./an(n))
+fN = lambda N : fn(n(N))    
+
+Heavi = lambda N : (1./2)*(numpy.sign(N)+1.)
+
+nm = lambda N : -n0*((A(N)/a0)**(1./p)-1)**(1./2)
+np = lambda N : n0*((A(N)/a0)**(1./p)-1)**(1./2)
+
+DHm = lambda N : -1.3887943864964e-16*N*(1.0*numpy.exp(N**2/2)**1.0 - 1.0)**(-0.5)*numpy.exp(N**2/2)**1.0/(1.0e-5*(1.0*numpy.exp(N**2/2)**1.0 - 1)**1.0 + 1.0e-5)**2 + 5.55517754598561e-21*N*(1.0*numpy.exp(N**2/2)**1.0 - 1.0)**0.5*numpy.exp(N**2/2)**1.0/(1.0e-5*(1.0*numpy.exp(N**2/2)**1.0 - 1)**1.0 + 1.0e-5)**3
+
+DHp = lambda N : +1.3887943864964e-16*N*(1.0*numpy.exp(N**2/2)**1.0 - 1.0)**(-0.5)*numpy.exp(N**2/2)**1.0/(1.0e-5*(1.0*numpy.exp(N**2/2)**1.0 - 1)**1.0 + 1.0e-5)**2 - 5.55517754598561e-21*N*(1.0*numpy.exp(N**2/2)**1.0 - 1.0)**0.5*numpy.exp(N**2/2)**1.0/(1.0e-5*(1.0*numpy.exp(N**2/2)**1.0 - 1)**1.0 + 1.0e-5)**3
+
+n = lambda N : Heavi(N)*np(N) + Heavi(-N)*nm(N)
+DH = lambda N : Heavi(N)*DHp(N) + Heavi(-N)*DHm(N)
+
+f = open('evolve_hk_test_3.dat','w')
+
+a0 = 1e-05
+n0 = numpy.exp(25)
+p = 1
+
+k0 = 10**(-10)*numpy.exp(-25)
+
+Nics = numpy.array([-4])
+for i in range(200):
+    Nics = opt.newton_krylov(lambda N : (k0)**2 - 1e+04*fN(N),Nics)
+
+print 'Nics =', Nics
+print 'we have Nics'
+
+Nshss = numpy.array([5])
+for i in range(200):
+    Nshss = opt.newton_krylov(lambda N : (k0)**2 - 1e+06*fN(N),Nshss)
+
+print 'Nshss =', Nshss
+print 'we have Nshss'
+
+hk0 = numpy.zeros(1,dtype=complex)
+hk0.real = (((2.*k0)**(1./2))*A(Nics))**(-1.)
+
+Dhk0 = numpy.zeros(1,dtype=complex)
+Dhk0.real = -(Nics/A(Nics))*((2*k0)**(-1./2))
+Dhk0.imag = -Nics*((k0/2)**(1./2))/(A(Nics)*A(Nics)*H(Nics))
+
+print 'starting Nics to Nshss'
+
+hk_array = numpy.array(hk0, dtype = complex)
+
+f.write(str(Nics)+"\t"+str(hk0)+"\n")
+
+npts = 5000
+step = (Nshss-Nics)/(npts)
+N = Nics
+while N < Nshss:
+    array = rk4_step(N, hk0, Dhk0, step)
+    hk0 = hk0 + array[1]
+    Dhk0 = Dhk0 + array[0]
+    hk_array = numpy.append(hk_array, hk0)
+    N += step
+    f.write(str(Nics)+"\t"+str(hk0.real)+"\t"+str(hk0.imag)+"\n")    
+
+print Nics, N-step, Nshss, hk0, Dhk0
+
+f.close()
+
+Nics = -10.57877019
+
+plt.cla()
+plt.hold(True)
+plt.semilogy(numpy.linspace(Nics,Nshss,len(hk_array[:4850])), numpy.absolute(hk_array[:4850]))
+plt.savefig('abs_hk_vs_N_k3.png')
+
+plt.cla()
+plt.hold(True)
+plt.plot(numpy.linspace(Nics,Nshss,len(hk_array[:4850])), hk_array.imag[:4850])
+plt.savefig('imag_hk_vs_N_k3.png')
---
+++ bouncing_model/power_spectrum_bouncing_model.py
@@ -0,0 +1,106 @@
+import numpy as numpy
+import matplotlib.pyplot as plt
+
+import scipy.optimize as opt
+
+def DDhk(k0, N, hk0, Dhk0):
+    return -((3.*N -(1./N) +(DH(N)/H(N)))*Dhk0 +(((k0*N)/(A(N)*H(N)))**2)*hk0)
+
+def rk4_step(k0, N, hk0, Dhk0, step):
+    F1 = Dhk0
+    f1 = DDhk(k0, N, hk0, Dhk0)
+    F2 = Dhk0 +f1*step/2.
+    f2 = DDhk(k0, N +step/2., hk0 +F1*step/2., Dhk0 +f1*step/2.)
+    F3 = Dhk0 +f2*step/2.
+    f3 = DDhk(k0, N +step/2., hk0 +F2*step/2., Dhk0 +f2*step/2.)
+    F4 = Dhk0 +f3*step
+    f4 = DDhk(k0, N +step, hk0 +F3*step, Dhk0 +f3*step)    
+
+    return numpy.array([(f1 +2*f2 +2*f3 +f4)*step/6.], dtype=complex), numpy.array([(F1 +2*F2 +2*F3 +F4)*step/6.], dtype=complex) # [Dhk, hk] update
+
+A = lambda N : a0*numpy.exp(N**2/2.)
+an = lambda n : a0*((1.+(n/n0)**2)**p)
+aN = lambda N : an(n(N))
+h = lambda n : (2.*a0*n/n0**2)*(1./an(n)**2)
+H = lambda N : h(n(N))
+
+fn = lambda n : (2.*a0/n0**2)*(1./an(n))
+fN = lambda N : fn(n(N))    
+
+Heavi = lambda N : (1./2)*(numpy.sign(N)+1.)
+
+nm = lambda N : -n0*((A(N)/a0)**(1./p)-1)**(1./2)
+np = lambda N : n0*((A(N)/a0)**(1./p)-1)**(1./2)
+
+DHm = lambda N : -1.3887943864964e-16*N*(1.0*numpy.exp(N**2/2)**1.0 - 1.0)**(-0.5)*numpy.exp(N**2/2)**1.0/(1.0e-5*(1.0*numpy.exp(N**2/2)**1.0 - 1)**1.0 + 1.0e-5)**2 + 5.55517754598561e-21*N*(1.0*numpy.exp(N**2/2)**1.0 - 1.0)**0.5*numpy.exp(N**2/2)**1.0/(1.0e-5*(1.0*numpy.exp(N**2/2)**1.0 - 1)**1.0 + 1.0e-5)**3
+
+DHp = lambda N : +1.3887943864964e-16*N*(1.0*numpy.exp(N**2/2)**1.0 - 1.0)**(-0.5)*numpy.exp(N**2/2)**1.0/(1.0e-5*(1.0*numpy.exp(N**2/2)**1.0 - 1)**1.0 + 1.0e-5)**2 - 5.55517754598561e-21*N*(1.0*numpy.exp(N**2/2)**1.0 - 1.0)**0.5*numpy.exp(N**2/2)**1.0/(1.0e-5*(1.0*numpy.exp(N**2/2)**1.0 - 1)**1.0 + 1.0e-5)**3
+
+n = lambda N : Heavi(N)*np(N) + Heavi(-N)*nm(N)
+DH = lambda N : Heavi(N)*DHp(N) + Heavi(-N)*DHm(N)
+
+f = open('power_spectrum_bouncing_model_test.dat','w')
+
+a0 = 1e-05
+n0 = numpy.exp(25)
+p = 1
+
+k_min = 1e-25
+k_max = 1e-8
+
+Nshss = 3.02287778 
+
+print 'lift off!' 
+
+k_vs_hk = numpy.zeros(1,dtype=complex)
+
+k0 = k_min
+while k0 < k_max:
+    print 'k0 = ', k0
+
+    Nics = numpy.array([-1])
+    for i in range(200):
+        Nics = opt.newton_krylov(lambda N : (k0)**2 - 1e+04*fN(N),Nics)
+
+    print Nics
+    
+    hk0 = numpy.zeros(1,dtype=complex)
+    hk0.real = (((2.*k0)**(1./2))*A(Nics))**(-1.)
+
+    Dhk0 = numpy.zeros(1,dtype=complex)
+    Dhk0.real = -(Nics/A(Nics))*((2*k0)**(-1./2))
+    Dhk0.imag = -Nics*((k0/2)**(1./2))/(A(Nics)*A(Nics)*H(Nics))
+    
+    hkm = numpy.array(hk0, dtype = complex)
+
+    print 'got Nics, hk0 and Dhk0'
+
+    npts = 20000
+    step = (Nshss-Nics)/(npts)
+    print 'starting from Nics'
+
+    N = Nics
+    while N < Nshss:
+        array = rk4_step(k0, N, hk0, Dhk0, step)
+        hk0 = hk0 + array[1]
+        Dhk0 = Dhk0 + array[0]
+        N += step
+
+    k_vs_hk = numpy.append(k_vs_hk, hk0)
+    print N-step, Nshss, hk0, Dhk0, Nics
+    print '\n'
+    
+    temp = 8*(k0)**3/(2*numpy.pi**2)*(numpy.absolute(hk0))**2
+    f.write(str(k0)+"\t"+str(hk0)+"\t"+str(temp)+"\n")  
+    
+    k0 = 10*k0
+
+k_list = numpy.array([10**(-25 + i) for i in range(17)])
+TPS = [8*(k_list[i])**3/(2*numpy.pi**2)*(numpy.absolute(k_vs_hk[i+1]))**2 for i in range(len(k_list))]
+print k_list, TPS
+
+f.close()
+
+plt.loglog(k_list, TPS)
+#plt.show()
+plt.savefig('power_spectrum_bouncing_model_test.png')
